ee9fb6acaa628c2f4245c906631cbd0e71f1163e
jeanmarcj <jmjbup@gmail.com>
2021-08-26 16:19:46 +0200

    Init. Create view

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>Sign Up page - under construction</h1>
+  <div class="wrapper">
+    
+    <header>
+      <LogoTitle msg = "Welcome on your expression app" />
+    </header>
+
+    <section class="container d-flex justify-content-center align-items-center pt-7 pb-4">
+      <SignUpForm title="Sign up" textIntro ="Registration takes less than a minute but gives you full control over your orders."/>
+    </section>
   </div>
+  
 </template>
+
+<script>
+// @ is an alias to /src
+// import NavBarLight from '@/components/NavBarLight.vue';
+import LogoTitle from '@/components/Layout/LogoTitle.vue';
+import SignUpForm from '@/components/Layout/SignUpForm.vue';
+
+export default {
+  components: {
+    // NavBarLight
+    LogoTitle,
+    SignUpForm
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    height: 100vh;
+    background-color: #f7f7fc;
+  }
+  .pt-7 {
+    padding-top: 6rem !important;
+  }
+
+</style>
