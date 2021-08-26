2be2e4a7fb412a01c78d4eb03685fd9a6546032c
jeanmarcj <jmjbup@gmail.com>
2021-08-26 16:20:15 +0200

    Feat. Update for tests

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    applicationName: 'Welcome on your Expression app',
+    userFistName: 'Jean-Marc',
+    userLastName: 'Juif',
+    userisAdmin: false,
+  },
+  getters: {
+    fullName: state => {
+      return `${state.userFistName} ${state.userLastName}`
+    }
   },
   mutations: {
+    saveUserFirstName(state, payload) {
+      state.userFirstName = payload;
+    }
   },
   actions: {
   },
