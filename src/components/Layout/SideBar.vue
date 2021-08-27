cae12c34f38ab470e8797d3769613e16141c23fa
jeanmarcj <jmjbup@gmail.com>
2021-08-27 21:31:13 +0200

    Init. Create component

---
+++ src/components/Layout/SideBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+      <aside>
+        <!-- Search -->
+        <SearchForm />
+        
+        <!-- Last 3 Posts -->
+        <div class="widget mb-5">
+          <h3 class="widget-title pb-1 text-start">Last posts</h3>
+          <LastPosts />
+        </div>
+        <!-- Total active Users -->
+        <hr>
+        <div class="widget mb-5">
+          <h3 class="widget-title pb-1 text-start">Total Published Post</h3>
+          <TotalPosts class="text-info fw-bolder" />
+        </div>
+      </aside>
+    </div>
+</template>
+
+<script>
+import SearchForm from '@/components/Layout/SearchForm.vue';
+import LastPosts from '@/components/Widgets/LastPosts.vue';
+import TotalPosts from '@/components/Widgets/TotalPosts.vue';
+
+
+export default {
+  name: 'SideBar',
+  components: {
+    SearchForm,
+    LastPosts,
+    TotalPosts,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.widget-title {
+    margin-bottom: 1.25rem;
+    font-size: 1.125rem;
+}
+
+.text-info {
+  color: #6a9bf4 !important;
+}
+
+</style>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="account">
+  <div class="wrapper">
+    <NavBarLight />
     <h1>Manage your account</h1>
   </div>
 </template>
+
+<script>
+import NavBarLight from '@/components/Layout/NavBarLight.vue';
+
+export default {
+  components: {
+    NavBarLight
+  }
+}
+
+</script>
