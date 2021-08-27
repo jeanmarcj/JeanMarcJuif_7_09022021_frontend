91866a6eea542d0270abdb0c246223079cbd70eb
jeanmarcj <jmjbup@gmail.com>
2021-08-27 21:31:49 +0200

    Init. Add navbar component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <div class="wrapper">
+    <NavBarLight />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import NavBarLight from '@/components/Layout/NavBarLight.vue';
+
+export default {
+  components: {
+    NavBarLight
+  }
+}
+
+</script>
