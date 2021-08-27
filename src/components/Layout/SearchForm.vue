0a5e83ed0527d0e491a3e32a33bc128798efdf74
jeanmarcj <jmjbup@gmail.com>
2021-08-27 21:30:12 +0200

    Init. Search Form component

---
+++ src/components/Layout/SearchForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="widget my-5">      
+      <h3 class="widget-title text-start">Search Blog</h3>
+      <div class="input-group">
+        <span class="input-group-text">
+          <i class="bi bi-search"></i>
+        </span>
+        <input class="form-control" type="text" placeholder="Search...">
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchForm'
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.widget-title {
+    margin-bottom: 1.25rem;
+    font-size: 1.125rem;
+}
+
+</style>
