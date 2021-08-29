ce8a72708a6c68316c6f36c4d35b2cd62f585728
jeanmarcj <jmjbup@gmail.com>
2021-08-29 15:47:41 +0200

    Feat. Add axios config

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:3000/'
 
 Vue.config.productionTip = false
 
