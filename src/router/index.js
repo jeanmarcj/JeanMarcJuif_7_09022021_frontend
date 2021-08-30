e30d4f7b32402dc6fe230927db37a7a3e31ed03d
jeanmarcj <jmjbup@gmail.com>
2021-08-30 18:54:18 +0200

    Feat. Route are restricted by auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -9,12 +10,16 @@ const routes = [
     // Log into the app
     path: '/',
     name: 'Home',
+    meta: { guest: true },
     component: Home
   },
   {
     path: '/about',
     name: 'About',
-    meta: { layout: "no-sidebar"},
+    meta: {
+      layout: "no-sidebar",
+      guest: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -24,42 +29,76 @@ const routes = [
     // Create a new User
     path: '/signup',
     name: 'SignUp',
+    meta: { guest: true },
     component: () => import(/* webpackChunkName: "signup" */ '../views/SignUp.vue')
   },
   {
     // View all published Posts (list)
     path: '/bloglist',
     name: 'Bloglist',
+    meta: { requiresAuth: true },
     component: () => import(/* webpackChunkName: "bloglist" */ '../views/BlogList.vue')
   },
   {
     // Create a post
     path: '/post/newpost',
     name: 'NewBlog',
+    meta: { requiresAuth: true },
     component: () => import(/* webpackChunkName: "bloglist" */ '../views/BlogCreate.vue')
   },
   {
     // Read a sigle Post
     path: '/post/:id',
     name: 'BlogSingle',
-    component: () => import(/* webpackChunkName: "bloglist" */ '../views/BlogSingle.vue')
+    meta: { requiresAuth: true },
+    component: () => import('../views/BlogSingle.vue')
   },
   {
     // Edit a single post
     path: '/blogedit/:id',
     name: 'Blogedit',
+    meta: { requiresAuth: true },
     component: () => import(/* webpackChunkName: "bloglist" */ '../views/BlogEdit.vue')
   },
   {
     // View the user account
     path: '/account',
     name: 'Account',
+    meta: { requiresAuth: true },
     component: () => import(/* webpackChunkName: "account" */ '../views/Account.vue')
   },
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// Unauthorized Users
+// Naviagtion guard to checks if a route has the meta: {requireAuth: true } key
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isAuthenticated) {
+      next();
+      return;
+    }
+    next('/');
+  } else {
+    next();
+  }
+});
+
+// Authorized Users
+// The meta: {guest: true} stops users that are logged in from accessing the routes with guest meta
+router.beforeEach((to, from, next) => {
+  if(to.matched.some((record) => record.meta.guest)) {
+    if (store.getters.isAuthenticated) {
+      next("/bloglist");
+      return;
+    }
+    next();
+  } else {
+    next();
+  }
 })
 
 export default router
